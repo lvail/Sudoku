3a9fb1a6fa1980e81d74b787e80eb35ec63d938e
lvail <lvail@CS-LVail-L.ONU-AD.olivet.edu>
2015-02-23 23:13:06 -0600

    Solves sample puzzle, difficulty 2 of 5 using strategy1 and strategy2. Quitting for tonight.

--- sudoku/src/sudoku/Sudoku.java
+++ sudoku/src/sudoku/Sudoku.java
@@ -12,18 +12,18 @@ import java.util.SortedSet;
 
 public class Sudoku {
 
-	// input for a 9 x 9 sudoku puzzle to solve, difficulty 1 of 5
+	// input for a 9 x 9 sudoku puzzle to solve, difficulty 2 of 5
 	String stringPuzzle = 
-			  "5  421   " +		
-			  "3   9 1 4" +
-			  " 71 3  2 " +
-			  "215  97 6" +
-			  "  35784  " +
-			  "8 76  395" +
-			  " 5  8 93 " +
-			  "1 8 6   7" +
-			  "   153  8" ;
-	
+			  "  8 72 96" +
+			  " 2 68   5" +
+			  "    31  2" +
+			  "  6 2 5  " +
+			  "   8 3   " +
+			  "  3 4 1  " +
+			  "4  39    " +
+			  "3   65 7 " +
+			  "26 71 8  " ;
+
 	// main data structure
 	private SortedSet<Integer>[][] puzzle = new TreeSet[9][9];
 	
@@ -33,6 +33,62 @@ public class Sudoku {
 		app.run();	// makes methods non-static
 	}
 	
+	// main entry point for app
+	private void run()
+	{
+		System.out.println("sudoku running...");
+		initPuzzle();
+		printPuzzle();
+		while (strategy1() && strategy2()) {
+		}
+		
+		System.out.println("-----------------------------------");
+		printPuzzle();
+	}
+
+	// initialize puzzle array to known and "could be" sets
+	private void initPuzzle() {
+		System.out.println("puzzleInit()...");
+		
+		SortedSet<Integer> U = new TreeSet<Integer>();	// U is the universal sudoku set of digits 1-9
+		U.add(1); U.add(2); U.add(3); U.add(4); U.add(5);
+		U.add(6); U.add(7); U.add(8); U.add(9);
+	
+		char ch;
+		int digit;
+	
+		// load stringPuzzle into 9x9 array puzzle
+		for (int r = 0; r < 9; r++) {
+			for (int c = 0; c < 9; c++) {
+				ch = stringPuzzle.charAt(r*9+c);
+				if (Character.isDigit(ch)) {
+					digit = Integer.parseInt(""+ch);
+					puzzle[r][c] = new TreeSet<Integer>();
+					puzzle[r][c].add(digit);
+				} else if (ch == ' ') {
+					puzzle[r][c] = new TreeSet<Integer>(U);
+				} else {
+					System.out.println("Bad character '" + ch + "' initilizing puzzle.");
+				}
+			}
+		}
+	}
+
+	private void printPuzzle() {
+		System.out.println("puzzlePrint()...");
+	
+		for (int r = 0; r < 9; r++) {
+			for (int c = 0; c < 9; c++) {
+				if (puzzle[r][c].size() == 1) {
+					System.out.print(puzzle[r][c].first() + " ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
 	// remove known row, column, block integers from each cell
 	private boolean strategy1() {
 		boolean changes = false;
@@ -52,6 +108,43 @@ public class Sudoku {
 			}
 		}
 		return changes;	
+	}
+
+	// returns set of known integers on row r
+	private SortedSet<Integer> rowSet(int r) {
+		SortedSet<Integer> set = new TreeSet<Integer>();
+		for (int c = 0; c < 9; c++) {
+			if (puzzle[r][c].size() == 1) {
+				set.add(puzzle[r][c].first());
+			}
+		}
+		return set;
+	}
+
+	// returns set of known integers on column c
+	private SortedSet<Integer> colSet(int c) {
+		SortedSet<Integer> set = new TreeSet<Integer>();
+		for (int r = 0; r < 9; r++) {
+			if (puzzle[r][c].size() == 1) {
+				set.add(puzzle[r][c].first());
+			}
+		}
+		return set;
+	}
+
+	// returns set of known integers in 3 x 3 block containing row r, column c
+	private SortedSet<Integer> blockSet(int r, int c) {
+		SortedSet<Integer> set = new TreeSet<Integer>();
+		int br = (r / 3) * 3;
+		int bc = (c / 3) * 3;
+		for (r = br; r < br + 3; r++) {
+			for (c = bc; c < bc + 3; c++) {
+				if (puzzle[r][c].size() == 1) {
+					set.add(puzzle[r][c].first());
+				}
+			}
+		}
+		return set;
 	}
 
 	// remove "could be" integers from every other row or column in each cell if only one possible
@@ -87,99 +180,6 @@ public class Sudoku {
 		return changes;
 	}
 	
-	// main entry point for app
-	private void run()
-	{
-		System.out.println("sudoku running...");
-		initPuzzle();
-		printPuzzle();
-		while (strategy1() && strategy2()) {
-		}
-		
-		System.out.println("-----------------------------------");
-		printPuzzle();
-	}
-
-	// initialize puzzle array to known and "could be" sets
-	private void initPuzzle() {
-		System.out.println("puzzleInit()...");
-		
-		SortedSet<Integer> U = new TreeSet<Integer>();	// U is the universal sudoku set of digits 1-9
-		U.add(1); U.add(2); U.add(3); U.add(4); U.add(5);
-		U.add(6); U.add(7); U.add(8); U.add(9);
-
-		char ch;
-		int digit;
-
-		// load stringPuzzle into 9x9 array puzzle
-		for (int r = 0; r < 9; r++) {
-			for (int c = 0; c < 9; c++) {
-				ch = stringPuzzle.charAt(r*9+c);
-				if (Character.isDigit(ch)) {
-					digit = Integer.parseInt(""+ch);
-					puzzle[r][c] = new TreeSet<Integer>();
-					puzzle[r][c].add(digit);
-				} else if (ch == ' ') {
-					puzzle[r][c] = new TreeSet<Integer>(U);
-				} else {
-					System.out.println("Bad character '" + ch + "' initilizing puzzle.");
-				}
-			}
-		}
-	}
-
-	private void printPuzzle() {
-		System.out.println("puzzlePrint()...");
-
-		for (int r = 0; r < 9; r++) {
-			for (int c = 0; c < 9; c++) {
-				if (puzzle[r][c].size() == 1) {
-					System.out.print(puzzle[r][c].first() + " ");
-				} else {
-					System.out.print("  ");
-				}
-			}
-			System.out.println();
-		}
-	}
-
-	// returns set of known integers on row r
-	private SortedSet<Integer> rowSet(int r) {
-		SortedSet<Integer> set = new TreeSet<Integer>();
-		for (int c = 0; c < 9; c++) {
-			if (puzzle[r][c].size() == 1) {
-				set.add(puzzle[r][c].first());
-			}
-		}
-		return set;
-	}
-	
-	// returns set of known integers on column c
-	private SortedSet<Integer> colSet(int c) {
-		SortedSet<Integer> set = new TreeSet<Integer>();
-		for (int r = 0; r < 9; r++) {
-			if (puzzle[r][c].size() == 1) {
-				set.add(puzzle[r][c].first());
-			}
-		}
-		return set;
-	}
-			
-	// returns set of known integers in 3 x 3 block containing row r, column c
-	private SortedSet<Integer> blockSet(int r, int c) {
-		SortedSet<Integer> set = new TreeSet<Integer>();
-		int br = (r / 3) * 3;
-		int bc = (c / 3) * 3;
-		for (r = br; r < br + 3; r++) {
-			for (c = bc; c < bc + 3; c++) {
-				if (puzzle[r][c].size() == 1) {
-					set.add(puzzle[r][c].first());
-				}
-			}
-		}
-		return set;
-	}
-
 	// returns set of other "could be" sets of integers on row except col
 	private SortedSet<Integer> rowSet2(int row, int col) {
 		SortedSet<Integer> T = new TreeSet<Integer>();
