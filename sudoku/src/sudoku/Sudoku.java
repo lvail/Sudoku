5385e68f2fb425d8289b8ef9c855d84243ef63d5
lvail <lvail@CS-LVail-L.ONU-AD.olivet.edu>
2015-02-23 20:52:25 -0600

    Working on puzzle level 2 difficulty.

--- sudoku/src/sudoku/Sudoku.java
+++ sudoku/src/sudoku/Sudoku.java
@@ -11,7 +11,8 @@ import java.util.TreeSet;
 import java.util.SortedSet;
 
 public class Sudoku {
-	
+
+	// main data structure
 	private SortedSet<Integer>[][] puzzle = new TreeSet[9][9];
 	
 	public static void main(String [] args)
@@ -23,62 +24,80 @@ public class Sudoku {
 	// main entry point for app
 	private void run()
 	{
-		System.err.println("sudoku running...");
-		puzzleInit();
-		puzzlePrint();
+		System.out.println("sudoku running...");
+		initPuzzle();
+		printPuzzle();
 		
-		// Test my set building methods
-//		System.out.println("rowSet(0) = " + rowSet(0));
-//		System.out.println("rowSet(8) = " + rowSet(8));
-//		System.out.println("colSet(0) = " + colSet(0));
-//		System.out.println("colSet(8) = " + colSet(8));
-//		System.out.println("blockSet(8, 0) = " + blockSet(8, 0));
-//		System.out.println("blockSet(4, 4) = " + blockSet(4, 4));
-//		System.out.println("blockSet(1, 7) = " + blockSet(1, 7));
-		
-		int round = 0;
-		boolean solved;
+		// strategy #1, simple row, column, block elimination to determine a single possible integer
+		boolean changes;
 		do {
-			solved = true;
-			System.err.println("round " + ++round);
+			changes = false;
 			for (int r = 0; r < 9; r++) {
 				for (int c = 0; c < 9; c++) {
+					System.out.println("["+(r+1)+(c+1)+"]="+puzzle[r][c]);
 					if (puzzle[r][c].size() > 1) {
-						solved = false;
-						System.out.println("Before: ["+r+"]["+c+"]="+puzzle[r][c]);
 						SortedSet<Integer> S = new TreeSet<Integer>();
 						S.addAll(rowSet(r));
 						S.addAll(colSet(c));
 						S.addAll(blockSet(r, c));
-						puzzle[r][c].removeAll(S);
-						System.out.println("After:  ["+r+"]["+c+"]="+puzzle[r][c]);
+						if (puzzle[r][c].removeAll(S)) {
+							changes = true;
+						}
 					}
 				}
-			} 
-		} while (!solved);
-		
-		puzzlePrint();
+			}
+			System.out.println();
+		} while (changes);
+
+		// strategy #2, remove "could be" integers from every cell in this row and column
+		do {
+			changes = false;
+			for (int r = 0; r < 9; r++) {
+				for (int c = 0; c < 9; c++) {
+					System.out.println("["+(r+1)+(c+1)+"]="+puzzle[r][c]);
+					if (puzzle[r][c].size() > 1) {
+						SortedSet<Integer> S = new TreeSet<Integer>();
+						S.addAll(puzzle[r][c]);
+						S.removeAll(rowSet2(r));
+						S.removeAll(colSet2(c));
+						if (S.size() == 1) {
+							puzzle[r][c].clear();
+							puzzle[r][c].addAll(S);
+							changes = true;
+							System.out.println("["+(r+1)+(c+1)+"]="+puzzle[r][c]);
+						}
+					}
+				}
+			}
+			System.out.println();
+		} while (changes);
+
+		System.out.println("-----------------------------------");
+		printPuzzle();
 	}
 
-	private void puzzleInit() {
-		System.err.println("puzzleInit()...");
+	// initialize puzzle array to known and "could be" sets
+	private void initPuzzle() {
+		System.out.println("puzzleInit()...");
 		
-		String stringPuzzle = "5  421   " +		// 9 x 9 sudoku puzzle to solve
-							  "3   9 1 4" +
-							  " 71 3  2 " +
-							  "215  97 6" +
-							  "  35784  " +
-							  "8 76  395" +
-							  " 5  8 93 " +
-							  "1 8 6   7" +
-							  "   153  8" ;
+		String stringPuzzle = "  8 72 96" +		// 9 x 9 sudoku difficulty 2 puzzle to solve
+							  " 2 68   5" +
+							  "    31  2" +
+							  "  6 2 5  " +
+							  "   8 3   " +
+							  "  3 4 1  " +
+							  "4  39    " +
+							  "3   65 7 " +
+							  "26 71 8  " ;
 		
 		SortedSet<Integer> U = new TreeSet<Integer>();	// U is the universal sudoku set of digits 1-9
 		U.add(1); U.add(2); U.add(3); U.add(4); U.add(5);
 		U.add(6); U.add(7); U.add(8); U.add(9);
-		
+
 		char ch;
 		int digit;
+
+		// load stringPuzzle into 9x9 array puzzle
 		for (int r = 0; r < 9; r++) {
 			for (int c = 0; c < 9; c++) {
 				ch = stringPuzzle.charAt(r*9+c);
@@ -94,14 +113,26 @@ public class Sudoku {
 				
 			}
 		}
+
+		printPuzzle();
+		
+		// remove known row, column, and 3x3 block integers from each incomplete position
+		for (int r = 0; r < 9; r++) {
+			for (int c = 0; c < 9; c++) {
+				if (puzzle[r][c].size() > 1) {
+					puzzle[r][c].removeAll(rowSet(r));
+					puzzle[r][c].removeAll(colSet(c));
+					puzzle[r][c].removeAll(blockSet(r,c));
+				}
+			}
+		}
 	}
 
-	private void puzzlePrint() {
-		System.err.println("puzzlePrint()...");
+	private void printPuzzle() {
+		System.out.println("puzzlePrint()...");
 
 		for (int r = 0; r < 9; r++) {
 			for (int c = 0; c < 9; c++) {
-//				System.out.print(puzzle[r][c].first() + " ");
 				if (puzzle[r][c].size() == 1) {
 					System.out.print(puzzle[r][c].first() + " ");
 				} else {
@@ -110,9 +141,9 @@ public class Sudoku {
 			}
 			System.out.println();
 		}
-		
 	}
 
+	// returns set of known integers on row r
 	private SortedSet<Integer> rowSet(int r) {
 		SortedSet<Integer> set = new TreeSet<Integer>();
 		for (int c = 0; c < 9; c++) {
@@ -123,6 +154,7 @@ public class Sudoku {
 		return set;
 	}
 	
+	// returns set of known integers on column c
 	private SortedSet<Integer> colSet(int c) {
 		SortedSet<Integer> set = new TreeSet<Integer>();
 		for (int r = 0; r < 9; r++) {
@@ -133,6 +165,7 @@ public class Sudoku {
 		return set;
 	}
 			
+	// returns set of known integers in 3 x 3 block containing row r, column c
 	private SortedSet<Integer> blockSet(int r, int c) {
 		SortedSet<Integer> set = new TreeSet<Integer>();
 		int br = (r / 3) * 3;
@@ -146,5 +179,23 @@ public class Sudoku {
 		}
 		return set;
 	}
-			
+
+	// returns set of "could be" sets of integers on row r
+	private SortedSet<Integer> rowSet2(int r) {
+		SortedSet<Integer> set = new TreeSet<Integer>();
+		for (int c = 0; c < 9; c++) {
+			set.addAll(puzzle[r][c]);
+		}
+		return set;
+	}
+	
+	// returns set of "could be" sets of integers on column c
+	private SortedSet<Integer> colSet2(int c) {
+		SortedSet<Integer> set = new TreeSet<Integer>();
+		for (int r = 0; r < 9; r++) {
+				set.addAll(puzzle[r][c]);
+		}
+		return set;
+	}
+
 }
